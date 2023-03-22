0b9f82017effa71c4fe91caeee6cefd7f037822a
bukuro <manzoku.bukuro@gmail.com>
2023-03-22 22:52:44 +0900

    現在のpathを取得してチャートに反映 サイドメニューを共通化

--- src/components/pages/Sleep.vue
+++ src/components/pages/Sleep.vue
@@ -1,7 +1,107 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+import SideMenu from '../uiParts/SideMenu.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+interface Chart {
+    type: string;
+    data: {
+        labels: string[];
+        datasets: {
+            data: number[];
+            backgroundColor: string;
+            borderColor?: string
+        }[];
+    };
+    options: {
+        plugins: {
+            legend: {
+                display: boolean;
+            };
+        };
+    };
+}
+const chartData = ref<{ [key: string]: Chart }>({
+    steps: {
+        type: 'bar',
+        data: {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+                {
+                    data: [10, 20, 5, 20, 40, 5],
+                    backgroundColor: '#17A220',
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        },
+    },
+    sleep: {
+        type: 'bar',
+        data: {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+                {
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: '#17A220',
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        },
+    },
+    heart: {
+        type: 'line',
+        data: {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+                {
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: '#17A220',
+                    borderColor: 'green'
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        },
+    },
+});
+const activeTab = ref<string>('steps');
+const changeTab = (chartKey: string) => {
+    activeTab.value = chartKey;
+};
+const currentPath = route.path.slice(1);
+onMounted(() => {
+    const canvas: any = document.getElementById(`canvas-${currentPath}`);
+    new Chart(canvas, (chartData as any).value[currentPath]);
+});
 </script>
 
 <template>
-    <div></div>
+    <div class="content-container">
+        <SideMenu />
+        <div class="category-container">
+            <div class="tab-content">
+                <canvas :id="`canvas-${currentPath}`"></canvas>
+            </div>
+        </div>
+    </div>
 </template>
---
+++ src/components/uiParts/SideMenu.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+interface Tab {
+    label: string;
+    chart: string;
+}
+const tabs = ref<Tab[]>([
+    { label: '歩数', chart: 'steps' },
+    { label: '睡眠', chart: 'sleep' },
+    { label: '心拍数', chart: 'heart' }
+]);
+const router = useRouter();
+const changeTab = (chartKey: string) => {
+    router.push(`/${chartKey}`);
+};
+</script>
+<template>
+    <div class="tab-container">
+        <button class="tab-links" v-for="(tab, tabIndex) in tabs" :key="`tab-${tabIndex}`" @click="changeTab(tab.chart)">
+            {{ tab.label }}
+        </button>
+    </div>
+</template>
